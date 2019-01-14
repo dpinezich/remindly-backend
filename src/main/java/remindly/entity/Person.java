82d65fe91bbd9331453971e72fb17490eafc2a4b
dpinezich <david.pinezich@gmail.com>
2019-01-14 22:23:55 +0100

    add some structure

--- src/main/java/remindly/entity/Person.java
+++ src/main/java/remindly/entity/Person.java
@@ -1,4 +1,4 @@
-package hello;
+package remindly.entity;
 
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
--- src/main/java/remindly/repository/CategoryRepository.java
+++ src/main/java/remindly/repository/CategoryRepository.java
@@ -1,10 +1,11 @@
-package hello;
+package remindly.repository;
 
 import java.util.List;
 
 import org.springframework.data.repository.PagingAndSortingRepository;
 import org.springframework.data.repository.query.Param;
 import org.springframework.data.rest.core.annotation.RepositoryRestResource;
+import remindly.entity.Category;
 
 @RepositoryRestResource(collectionResourceRel = "category", path = "category")
 public interface CategoryRepository extends PagingAndSortingRepository<Category, Long> {
